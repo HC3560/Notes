13ca7681a4acfa075b11a816a47b0b2db61aea0c
HC3560 <hc3560@qq.com>
2023-02-10 18:50:13 +0800

    Update FPGA Notes

--- Class/FPGA/class_hdmi/class_hdmi.runs/impl_1/rundef.js
+++ Class/FPGA/class_hdmi/class_hdmi.runs/impl_1/rundef.js
@@ -23,7 +23,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 // pre-commands:
-ISETouchFile( "init_design", "begin" );
+ISETouchFile( "write_bitstream", "begin" );
 ISEStep( "vivado",
          "-log src.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source src.tcl -notrace" );
 
--- Class/FPGA/class_hdmi/class_hdmi.srcs/sim_1/new/tb_src.v
+++ Class/FPGA/class_hdmi/class_hdmi.srcs/sim_1/new/tb_src.v
@@ -20,35 +20,34 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module tb_src(
-//    reg sys_clk;
-//    wire hdmi_vs,hdmi_hs,hdmi_de,hdmi_clk;
-//    wire [7:0] hdmi_r,hdmi_g,hdmi_b;
-//    wire hdmi_rst_n;
-//    wire hdmi_scl,hdmi_sda;
+module tb_src();
+    reg sys_clk;
+    wire hdmi_vs,hdmi_hs,hdmi_de,hdmi_clk;
+    wire [7:0] hdmi_r,hdmi_g,hdmi_b;
+    wire hdmi_rst_n;
+    wire hdmi_scl,hdmi_sda;
     
-////    color_bar U1(
-//    image_processing U1(
-//        .hdmi_clk(hdmi_clk),
-////        .rst(rst),
-//        .hdmi_vs(hdmi_vs),
-//        .hdmi_hs(hdmi_hs),
-//        .hdmi_de(hdmi_de),
-//        .hdmi_r(hdmi_r),
-//        .hdmi_g(hdmi_g),
-//        .hdmi_b(hdmi_b),
-//        .hdmi_rst_n(hdmi_rst_n),
-//        .hdmi_scl(hdmi_scl),
-//        .hdmi_sda(hdmi_sda)
-//        );
+//    color_bar U1(
+    src U1(
+        .hdmi_clk(hdmi_clk),
+//        .rst(rst),
+        .hdmi_vs(hdmi_vs),
+        .hdmi_hs(hdmi_hs),
+        .hdmi_de(hdmi_de),
+        .hdmi_r(hdmi_r),
+        .hdmi_g(hdmi_g),
+        .hdmi_b(hdmi_b),
+        .hdmi_rst_n(hdmi_rst_n),
+        .hdmi_scl(hdmi_scl),
+        .hdmi_sda(hdmi_sda)
+        );
     
+    initial begin
+//        rst = 1'b0;
+        sys_clk = 1'b0;
+    end
 //    initial begin
-////        rst = 1'b0;
-//        sys_clk = 1'b0;
+//        #10 rst = 1'b1;
 //    end
-////    initial begin
-////        #10 rst = 1'b1;
-////    end
-//    always #5 sys_clk=~sys_clk; 
-   );
+    always #5 sys_clk=~sys_clk; 
 endmodule
